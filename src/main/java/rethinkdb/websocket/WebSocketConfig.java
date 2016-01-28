984488fc1aedec4ae7b1a34a147b8a8066737aa2
geowarin <geowarin@gmail.com>
2016-01-28 23:23:08 +0100

    Remove unused config

--- src/main/java/rethinkdb/websocket/WebSocketConfig.java
+++ src/main/java/rethinkdb/websocket/WebSocketConfig.java
@@ -13,7 +13,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/ws");
     }
 
     @Override
