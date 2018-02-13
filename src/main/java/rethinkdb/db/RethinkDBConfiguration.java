d686c78aad5d86ae1f1d5b1002b1651d2fe2d6b4
Mohiuddin Ahmed <ahmed.mohi.duet@gmail.com>
2018-02-14 01:18:46 +0600

    moving dbhost config to global app props. Build for non docker environment mistakenly showing a db connection timeout error

--- src/main/java/rethinkdb/db/RethinkDBConfiguration.java
+++ src/main/java/rethinkdb/db/RethinkDBConfiguration.java
@@ -1,12 +1,24 @@
 package rethinkdb.db;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class RethinkDBConfiguration {
-    // connect to docker
-    public static final String DBHOST = "192.168.99.100";
+
+    @Autowired
+    private Environment env;
+
+    public static String DBHOST = "127.0.0.1";
+
+    @PostConstruct
+    public void init() {
+        this.DBHOST = this.env.getProperty("rethinkdb.dbhost");
+    }
 
     @Bean
     public RethinkDBConnectionFactory connectionFactory() {
